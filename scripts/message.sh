ea03ec743b09c65c9052286543b67dd0b36bd97b
Luis Fernando <lcruzf@intercorp.com.pe>
2025-02-11 19:52:05 -0500

    update: message.sh

--- scripts/message.sh
+++ scripts/message.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Inicio del script message.sh....."
+
+if [ -Z "$MENSAJE" ]; then
+  echo "NO existe la variable del MENSAJE"
+  exit 1
+fi
+
 echo "Valor de mensaje recibido: $MENSAJE"
 
 echo finalizando script.
